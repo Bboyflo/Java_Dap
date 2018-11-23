a31a6d32d918e025f5e2d6767a39f0bcdf498488
Bboyflo <florian.branchereau@live.fr>
2018-11-24 00:07:46 +0100

    Update Microsoft

--- dap/src/main/java/fr/ynov/dap/dap/microsoft/auth/TokenService.java
+++ dap/src/main/java/fr/ynov/dap/dap/microsoft/auth/TokenService.java
@@ -22,6 +22,25 @@ public interface TokenService {
      */
     @FormUrlEncoded
     @POST("/{tenantid}/oauth2/v2.0/token")
+    Call<TokenResponse> getAccessTokenFromAuthCode(
+            @Path("tenantid") String tenantId,
+            @Field("client_id") String clientId,
+            @Field("client_secret") String clientSecret,
+            @Field("grant_type") String grantType,
+            @Field("code") String code,
+            @Field("redirect_uri") String redirectUrl);
+
+    /**
+     * @param tenantId .
+     * @param clientId .
+     * @param clientSecret .
+     * @param grantType .
+     * @param code .
+     * @param redirectUrl .
+     * @return .
+     */
+    @FormUrlEncoded
+    @POST("/{tenantid}/oauth2/v2.0/token")
     Call<TokenResponse> getAccessTokenFromRefreshToken(
             @Path("tenantid") String tenantId,
             @Field("client_id") String clientId,
